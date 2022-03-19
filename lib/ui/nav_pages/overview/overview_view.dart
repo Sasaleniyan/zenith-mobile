00c83c95830f8ca53e04024b56b4fbd408beaf63
amNinE <sasaleniyano@gmail.com>
2022-03-19 01:57:56 +0100

    project file structure

---
+++ lib/ui/nav_pages/overview/overview_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class OverviewPage extends StatelessWidget {
+  const OverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: const Text(
+          "Still under construction",
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
